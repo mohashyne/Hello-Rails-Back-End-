e1639d5424ac99ff79ff05396a31b54b90d9d88f
MSA <crystalbluetech@gmail.com>
2023-10-31 21:07:14 +0100

    define api controllers and route endpoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+message1 = Message.create(content: 'Hello, World!')
+message2 = Message.create(content: 'Greetings!')
+message3 = Message.create(content: 'Welcome!')
+message4 = Message.create(content: 'Hi Code Reviewer!')
+message5 = Message.create(content: 'Good day Happy Coding!')
